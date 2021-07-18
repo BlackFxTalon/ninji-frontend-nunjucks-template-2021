7b066aae3ce01907930292e123e9b3f80011ab18
blackfxtalon <andyv@inbox.ru>
2021-07-18 23:59:58 +0500

    template is done

---
+++ README.md
@@ -0,0 +1,21 @@
+# ninji-frontend-nunjucks-template-2021
+
+my new template for frontend development in 2021 (gulp + webpack + sass + nunjucks)
+
+## Steps to start developing your new project with this template:
+
+## clone project
+
+<code>git clone https://github.com/BlackFxTalon/ninji-frontend-template-2021</code>
+
+## install dependencies
+
+<code>npm i</code>
+
+## launch development
+
+<code>npm run dev</code>
+
+## launch production
+
+<code>npm run build</code>
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+    plugins: [
+        new webpack.ProvidePlugin({
+            jQuery: "jquery",
+            "window.jQuery": "jquery",
+            jquery: "jquery",
+            "window.jquery": "jquery",
+            $: "jquery",
+            "window.$": "jquery",
+        }),
+    ],
+
+    entry: {
+        vendors: "./src/js/vendors/vendors.js",
+        main: "./src/js/main.js",
+    },
+
+    output: {
+        filename: "[name].js",
+        chunkFilename: "[name].js",
+        publicPath: "/",
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    query: {
+                        presets: [["@babel/preset-env", { modules: false }]],
+                    },
+                },
+            },
+        ],
+    },
+
+    resolve: {
+        alias: {
+            "%components%": path.resolve(__dirname, "src/components"),
+        },
+    },
+};
