0674c8bae942a5daaa9974136ba694d1fa251a9a
blackfxtalon <andyv@inbox.ru>
2022-04-02 19:20:21 +0500

    restructured js

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,21 @@
-/* main.js */
-import "./import/components";
+/* alpine.js */
+
+import Alpine from 'alpinejs'
+ 
+window.Alpine = Alpine
+ 
+Alpine.start()
+
+
+/* swiper.js */
+
+// core version + navigation, pagination modules:
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+// custom scripts
+
+// init Swiper:
+const swiper = new Swiper('.swiper', {
+  // configure Swiper to use modules
+  modules: [Navigation, Pagination],
+});
